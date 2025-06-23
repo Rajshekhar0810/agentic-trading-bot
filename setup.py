601058fe3b94164e8245a6b317012961d0b5af05
Raj Shekhar <rajshekhar0810@gmail.com>
2025-06-23 15:57:54 +0530

    updated skeleton of code

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages    #setuptools: A Python module used
                                                #find_packages(): Automatically finds all the Python packages (i.e., folders with __init__.py) in your project
 
 setup(name = "agentic-trading-system",
-      version=0.0.1,
+      version= "0.0.1",
       author_email= "rajshekhar0810@gmail.com",
       author= "Raj Shekhar",
       description= "A trading system that uses agentic AI to trade in the stock market",
       packages=find_packages(),
-      install_requires=['lancedb','langchain','langgraph','tavily-python','polygon','langchain']
+      install_requires=['lancedb','langchain','langgraph','tavily-python','polygon']
       )
